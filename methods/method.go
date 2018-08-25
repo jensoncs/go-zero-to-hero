9d66cecdd2c50ff91d5e69af0c8848a95b9ff60c
Jenson <jensonc8@gmail.com>
2018-08-25 17:16:19 +0530

    Add methods example

---
+++ methods/method.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Employ struct {
+	name, currency string
+	salary         int
+}
+
+func main() {
+
+	emp1 := Employ{
+		name:     "ram",
+		currency: "INR",
+		salary:   100000,
+	}
+	emp1.display()
+}
+
+func (e *Employ) display() {
+	fmt.Println(e.name)
+}
